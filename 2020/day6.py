322f7bf90ab88f252774048851d86f1f3080dfc1
Wolfgang Steininger <wolfgang.steininger@admiral.at>
2020-12-08 10:35:05 +0100

    day 6 second puzzle

--- 2020/day6.py
+++ 2020/day6.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+
 def getdata(filename):
     values = []
     buffer = []
@@ -13,13 +16,24 @@ def getdata(filename):
     values.insert(len(values), buffer)
     return values
 
+
 def countpositiveanswers(answers):
-    group =0
+    pos = 0
     for answersgroup in answers:
-        group += len(set(''.join([i for i in answersgroup])))
-    return group
+        pos += len(set(''.join([i for i in answersgroup])))
+    return pos
 
 
+def countoverlappinganswers(answers):
+    all = 0
+    for answersgroup in answers:
+        pos = (''.join([i for i in answersgroup]))
+        countedpos = Counter(pos)  # Counter counts all chars and creates a dict with char as key and sum as value
+        onlyallpos = Counter(countedpos.values())[len(answersgroup)]  # count only those where the sum meets the size of the group
+        all += onlyallpos
 
-print (f'puzzle 1 = {countpositiveanswers(getdata("input_day6"))}')
+    return all
 
+
+print(f'puzzle 1 = {countpositiveanswers(getdata("input_day6"))}')
+print(f'puzzle 2 = {countoverlappinganswers(getdata("input_day6"))}')
