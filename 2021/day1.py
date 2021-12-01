fa34a94f52851e909795a2613e0fed3f03ce4568
wolfgang steininger <wolfgang.steininger@rbinternational.com>
2021-12-01 10:45:38 +0100

    solution day1

---
+++ 2021/day1.py
@@ -0,0 +1,106 @@
+class puzzle1():
+
+    def getinput(self, inputstring):
+        values = map(int,str.splitlines(inputstring))
+        return list(values)
+
+    def calculatedepth(self, measurements):
+        start = "None"
+        last = None
+        largerCounter = 0
+        
+        for me in measurements:
+           
+            
+            if (last is None):
+                last = me
+                #print (f"{me} {largerCounter}")
+                continue
+            
+            if (last < me):
+                largerCounter += 1
+            
+            #print (f"{me} {largerCounter}")
+            last = me
+        
+        return largerCounter
+
+    def getResult(self, inputString):
+        return self.calculatedepth(self.getinput(inputString))
+
+class window:
+    C = None
+    B = None
+    A = None
+    
+    def sum(self):
+        if (self.A is None or self.B is None or self.C is None):
+            return -1
+        return self.A + self.B + self.C
+    
+class puzzle2():
+
+    def getinput(self, inputstring):
+        values = map(int,str.splitlines(inputstring))
+        return list(values)
+
+    def calculatedepth(self, measurements):
+        start = "None"
+        last = None
+        largerCounter = 0
+        win = window()
+        
+        for me in measurements:
+            win.C = win.B
+            win.B = win.A
+            win.A = me
+            
+            if (win.A is None or win.B is None or win.C is None):
+                #print (f"{win.sum()} {largerCounter}")
+                continue
+            
+            if (last is not None):
+                if (last < win.sum()):
+                    largerCounter += 1
+            
+            #print (f"{win.sum()} {largerCounter}")
+            last = win.sum()
+        
+        return largerCounter
+
+    def getResult(self, inputString):
+        return self.calculatedepth(self.getinput(inputString))
+
+ 
+class test:
+    testinput = """199
+    200
+    208
+    210
+    200
+    207
+    240
+    269
+    260
+    263"""
+
+    testresult1 = 7
+    testresult2 = 5
+    
+    tester = puzzle1()
+    print(f"result t1={testresult1 == tester.getResult(testinput)}")
+    tester = puzzle2()
+    print(f"result t2={testresult2 == tester.getResult(testinput)}")
+    
+    
+    
+
+
+test()
+
+f = open(".\input_day1","r")
+values = f.read()
+
+print(puzzle1().getResult(values), end = "; ")
+
+print(puzzle2().getResult(values), end = "; ")
---
+++ 2021/testcode.py
@@ -0,0 +1,28 @@
+
+class Fibonacci:
+    def __init__(self, max_n):
+        self.MaxN = max_n
+        self.N = 0
+        self.A = 0
+        self.B = 0
+        
+    def __iter__(self):
+        self.N = 0
+        self.A = 0
+        self.B = 1
+        return self
+    
+    def __next__(self):
+        if (self.N < self.MaxN):
+            self.N += 1
+            self.A, self.B = self.B, self.A + self.B
+            return self
+        raise StopIteration
+
+
+s = Fibonacci(12)
+
+for f in s:
+    #print(f, end=" ")
+    print(f"A: {f.A} B: {f.B}")
+    
